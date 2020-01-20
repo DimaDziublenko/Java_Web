aeb4775487a3abe174f23a79648d4ebcbcb3cc7c
DimaDziublenko <56675165+DimaDziublenko@users.noreply.github.com>
2020-01-20 13:26:47 +0200

    hw4 have been created (#3)
    * hw4 have been created
    
    * <bagFix>
    - removed method toString() and add annotation @ToString

---
+++ WebApp/src/main/java/app/entities/User.java
@@ -0,0 +1,20 @@
+package app.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    private String name;
+    private String password;
+}
